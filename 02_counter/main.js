effd62f2a48ca90d575318314466dec7c12ec3af
bambxxz1e <s2427@e-mirim.hs.kr>
2026-03-24 10:01:12 +0900

    counter HTML id, class, JS addEventListener(), onclick

---
+++ 02_counter/main.js
@@ -0,0 +1,67 @@
+// HTML -> JS
+// 숫자 증가
+// 숫자 표시
+
+// 내가 짠 코드
+// const h1 = document.getElementById("h1");
+// const addBtn = document.getElementById("addBtn");
+
+// addBtn.addEventListener('click', () => {
+//     h1.innerText = Number(h1.innerText) + 1;
+// })
+
+// const resultH1 = document.querySelectorAll("h1")[0]
+
+let count = 0;
+
+// ----------------------------------
+
+// 1번
+// const resultH1 = document.getElementsByTagName("h1")[0];
+// const plusButton = document.getElementsByTagName("button")[0];
+
+// plusButton.addEventListener("click", () => {
+//         count++;
+        
+//         resultH1.innerHTML = count;
+//     }   
+// );
+
+// -----------------------------------
+
+// 2번
+// const resultH1 = result
+// const resultH1 = document.querySelector("#result")
+// const resultH1 = document.getElementById("result");
+// const plusButton = document.getElementsByClassName("plusBtn")[0];
+
+// plusButton.onclick = () => {
+//     count++;
+        
+//     resultH1.innerHTML = count;
+// } ;
+
+// -----------------------------------
+
+// 3번
+const resultH1 = document.getElementById("result");
+
+function plus(){
+    count++;
+        
+    resultH1.innerHTML = count;
+}
+
+// -----------------------------------
+
+// 함수를 호출해버리면 안됨
+// plusButton.addEventListener("click", 처리함수());
+// function 처리함수(){
+//      count++;
+//      resultH1.innerHTML = count;
+// }
+
+// 익명 함수 실행
+// (function (){
+// })()
+
