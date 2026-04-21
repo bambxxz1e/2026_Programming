51faaa03382e295c4d1c942287e286fec4b099d7
bambxxz1e <s2427@e-mirim.hs.kr>
2026-04-21 10:10:27 +0900

    TodoListApp() toggleTodo()

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,44 +1,55 @@
 // https://2026-programming.s2427.workers.dev/
 
 import { useState } from "react";
-import './todolist.css';
+import "./todolist.css";
 import Button from "./components/Button.jsx";
 import Checkbox from "./components/Checkbox.jsx";
-import TodoItemEmpty from './components/TodoItemEmpty.jsx'
-import TodoHeader from './components/TodoHeader.jsx'
-import TodoAdder from './components/TodoAdder.jsx'
-import TodoItem from './components/TodoItem.jsx'
-import TodoList from './components/TodoList.jsx'
+import TodoItemEmpty from "./components/TodoItemEmpty.jsx";
+import TodoHeader from "./components/TodoHeader.jsx";
+import TodoAdder from "./components/TodoAdder.jsx";
+import TodoItem from "./components/TodoItem.jsx";
+import TodoList from "./components/TodoList.jsx";
 
-class Todo{
-  constructor(id, text, isCompleted){
+class Todo {
+  constructor(id, text, isCompleted) {
     this.id = id;
     this.text = text;
     this.isCompleted = isCompleted;
   }
 }
 
-function TodoListApp(){
+function TodoListApp() {
   const [todos, setTodos] = useState([]);
 
-  function addTodo(text){
+  function addTodo(text) {
     // 이전 todos에 새로운걸 만들어서 추가하자!! -> 그걸 setTodos하자스!!
 
     setTodos((todos) => [
-      ...todos,     // todos에 있는 item을 다 꺼내서 새 리스트에 다시 넣어!@
+      ...todos, // todos에 있는 item을 다 꺼내서 새 리스트에 다시 넣어!@
       new Todo(
         Date.now(), // id : 고유 ID 시간을 이용 (new Date().getTime())
-        text,       // text : 할 일
-        false       // isCompleted : 할 일 완료 여부, 초기값 false
-      )
+        text, // text : 할 일
+        false, // isCompleted : 할 일 완료 여부, 초기값 false
+      ),
     ]);
   }
 
-  return(
+  function toggleTodo(id) {
+    setTodos((todos) =>
+      // todos에서 하나씩 꺼내서 todo라고 하기, 꺼낸 todo.id가 id와 같다면
+      // 새 객체 만들어서 값 복사하고 속성 수정
+
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    )
+  }
+
+  return (
     <div className="todo">
-      <TodoHeader /> 
-      <TodoAdder addTodo={addTodo}/>
-      <TodoList todos={todos}/>
+      <TodoHeader />
+      <TodoAdder addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
     </div>
   );
 }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,17 @@
 import Checkbox from "./Checkbox.jsx";
 import Button from "./Button.jsx";
 
-export default function TodoItem({todo}){
+export default function TodoItem({todo, toggleTodo}){
     return(
         // todo.isComplete가 true면 " todo__item--complete" / false면 ""
-        <li className={`todo__item${todo.isComplete ? " todo__item--complete" : ""}`}>
-          <Checkbox id={todo.id}>{todo.text}</Checkbox>
-          <Button className="todo__button todo__button--edit">🖋️</Button>
-          <Button className="todo__button todo__button--delete">🗑️</Button>
+        <li className={`todo__item${todo.isCompleted ? " todo__item--complete" : ""}`}>
+            <Checkbox 
+                id={todo.id} 
+                checked={todo.isCompleted}
+                onChange={() => toggleTodo(todo.id)}
+            >{todo.text}</Checkbox>
+            <Button className="todo__button todo__button--edit">🖋️</Button>
+            <Button className="todo__button todo__button--delete">🗑️</Button>
         </li>
     );
 }
