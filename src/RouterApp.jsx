61359597ec41ad19fd2a119dfd472554c4761ffa
bambxxz1e <s2427@e-mirim.hs.kr>
2026-05-14 14:43:19 +0900

    RouterApp BrowserApp, Routes, Route, Link

---
+++ src/RouterApp.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import CounterApp from "./CounterApp";
+import TodoList from "./components/TodoList";
+import TodoListApp from "./TodoListApp";
+
+function LinkButtonPageApp(){
+    return(
+        <div>
+            <h1>App 목록</h1>
+            <ul>
+                <li><Link to="/counterapp">🔢 CounterApp</Link></li>
+                <li><Link to="/todolistapp">📒 TodoListApp</Link></li>
+            </ul>
+        </div>
+    )
+}
+
+export default function RouterApp(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LinkButtonPageApp />}></Route>
+                <Route path="/counterapp" element={<CounterApp />}></Route>
+                <Route path="/todolistapp" element={<TodoListApp />}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
