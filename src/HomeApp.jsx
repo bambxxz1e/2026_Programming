102a37005df4e3f5e7220678abdd35771f3e43ee
bambxxz1e <s2427@e-mirim.hs.kr>
2026-05-14 13:48:03 +0900

    HomeApp, style={{}}

---
+++ src/HomeApp.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import CounterApp from './CounterApp';
+import TodoListApp from './TodoListApp';
+
+function ButtonPageApp({setPage}) {
+    return(
+        <>
+            <h1>App 목록</h1>
+            <ul>
+                <li>
+                    <button
+                        onClick={() => setPage('counterapp')}
+                        style={{fontSize: "1.1rem", width: "100px", height: "100px"}}>🔢<br/>CounterApp</button>
+                </li>
+                <li>
+                    <button 
+                        onClick={() => setPage('todolistapp')}
+                        style={{fontSize: "1.1rem", width: "100px", height: "100px"}}>📒<br/>TodoListApp</button>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default function HomeApp(){
+    const [page, setPage] = useState('home');
+    return(
+        <>
+            {/* page가 home이면 <ButtonPageApp /> */}
+            {page === 'home' && <ButtonPageApp setPage={setPage} />}
+
+            {/* page가 home이 아니면 home으로 가는 버튼 만들기! */}
+            {page !== 'home' &&
+                <button 
+                    onClick={() => setPage('home')}
+                    style={{
+                        position: 'fixed',
+                        left: '10px',
+                        bottom: '10px',
+                        cursor: 'pointer',
+                        borderRadius: '8px',
+                        backgroundColor: '#eee',
+                        border: 'none',
+                        padding: '6px',
+                    }}
+                    
+                >🏠HOME!!!</button>
+            }
+
+            {/* page가 counterapp이면 <CounterApp /> */}
+            {page === 'counterapp' && <CounterApp />}
+
+            {/* page가 todolistapp이면 <TodoListApp /> */}
+            {page === 'todolistapp' && <TodoListApp />}
+
+        </>
+    )
+}
